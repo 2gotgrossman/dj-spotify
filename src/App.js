19680bc96b92a088c1099945a699d0484c2f905a
David Grossman <2gotgrossman@gmail.com>
2020-01-03 15:01:48 -0800

    Adjusted images, text, and login:  button on homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import { Image } from 'react-bootstrap';
 import * as $ from "jquery";
 import "../node_modules/hash.js";
 import RecentlyPlayedList from "./RecentlyPlayedList";
 import logo from "./logo.jpg";
+import logo_lower from "./logo-lower.jpg";
+
 
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -33,6 +36,7 @@ const hash = window.location.hash
   }, {});
 
 window.location.hash = "";
+
 
 
 class App extends Component {
@@ -84,32 +88,54 @@ class App extends Component {
     });
   }
 
+
+
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           {!this.state.token && (
-            <a
-              className="btn btn--loginApp-link"
-              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                "%20"
-              )}&response_type=token&show_dialog=true`}
-            >
-              Login to Spotify
-            </a>
-          )}
+            <div>
+              <div className='containerHomePage' style={{backgroundImage: `url(${logo})`}}>
+                  <div class="content">
+                    <p className='headerText'>Spotify DJ</p>
+                    <p className='subtitleText'>DJ that house show now and <br /> create a playlist from your recently <br />played songs afterwards.</p>
+                  </div>
+                  <a
+                    className="myButton"
+                    href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                      "%20"
+                    )}&response_type=token&show_dialog=true`}
+                  >
+                    Login to Spotify
+                  </a>
+              </div>
+
+            </div>
+           )}
+
           {this.state.token && (
-            <RecentlyPlayedList
-              recently_played={this.state.recently_played}
-              token={this.state.token}
-            />
+            <div >
+               <div className='containerSongsPage' style={{backgroundImage: `url(${logo_lower})`}}>
+               <div class="content">
+                 <p className='subtitleText'>1. Select start song <br/>
+                                             2. Select end songs <br/>
+                                             3. Create playlist!</p>
+               </div>
+
+               </div>
+              <RecentlyPlayedList
+                recently_played={this.state.recently_played}
+                token={this.state.token}
+              />
+            </div>
           )}
         </header>
       </div>
     );
   }
 }
+
 
 export default App;
